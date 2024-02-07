8d750a9dbf588fab55c31e19fca94f437200e994
Mariano Rossini <mrossini@waypoint.la>
2024-02-07 11:09:58 -0300

    gateway

--- usuario-service/usuario.service/src/main/java/com/usuario/service/feignclient/CarFeignClient.java
+++ usuario-service/usuario.service/src/main/java/com/usuario/service/feignclient/CarFeignClient.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "car-service", url = "http://localhost:8002")
+//Con API GATEWAY ya no hace falta la url
+//@FeignClient(name = "car-service", url = "http://localhost:8002")
+@FeignClient(name = "car-service")
 public interface CarFeignClient {
 
     @PostMapping("/car")
